45ddbb331d82b19a4f724e44961fa14de0428b2a
sebastianroja <sebastian.android.norambuena@gmail.com>
2024-05-01 22:06:47 -0400

    css y ajustando el inicio de sesion con firebase

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 
 import React from 'react'
+import appFirebase from '../credenciales'
+import {getAuth,signOut} from 'firebase/auth'
 
- const Home = () => {
+const auth = getAuth(appFirebase)
+ const Home = ({correoUsuario}) => {
   return (
     <div>
-        <h2>Inicio</h2>
+        <h2 className='text-center'>Bienvenido Usuario:  {correoUsuario} <button className='btn btn-primary' onClick={()=>signOut(auth)}> Logout</button></h2>
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,58 @@
 /* eslint-disable no-unused-vars */
 
-import React from 'react'
+import React, { useState } from 'react'
 import Imagen from '../assets/Mobile.png'
 import imagenProfile from '../assets/profile1.jpg'
+import appFirebase from '../credenciales'
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
+const auth = getAuth(appFirebase)
 
 const Login = () => {
-  return (
+    const [registrando, setRegistrando]= useState (false) 
+
+    const functAutenticacion = async(e)=>{
+        e.preventDefault();
+        const correo = e.target.email.value;
+        const contraseña = e.target.password.value;
+        
+        if (registrando) {
+            try{
+                await createUserWithEmailAndPassword(auth, correo, contraseña)
+            }catch (error){
+                alert("Asegurese que la contraseña tenga mas de 8 caracteres")
+            }
+        }
+        else{
+            try{
+            await signInWithEmailAndPassword(auth, correo, contraseña)
+            } catch (error) {
+                alert("El correo o la contraseña son incorrectos")
+            }
+        }
+
+
+    }
+    
+    return (
     <div className='container'>
         <div className="row">
-            <div className="col-md4">
+            {}
+            <div className="col-md-4">
                 <div className="padre">
-                    <div className="card card-body">
-                        <form>
-                         <input type="text" placeholder='Ingresar Email'/>
-                         <input type="password" placeholder='Ingresar Contraseña' />
-                         <button>Registrarse</button>
+                    <div className="card card-body shadow">
+                        <img src={imagenProfile} alt=""  className='estilo-profile'/>
+                        <form onSubmit={functAutenticacion}>
+                         <input type="text" placeholder='Ingresar Email' className='cajatexto'id='email'/>
+                         <input type="password" placeholder='Ingresar Contraseña' className='cajatexto' id='password' />
+                         <button className='btnform'>{registrando ? "Registrate" : "Inicia sesion"}</button>
                         </form>
+                        <h4 className='texto'>{registrando ? "Si ya tienes cuenta" : "No tienes cuenta"}<button className='btnswitch' onClick={()=>setRegistrando(!registrando)}>{registrando ? "Inicio Sesion" : "Registrate"}</button></h4>
 
                     </div>
                 </div>
             </div>
 
-            <div className="col-md8">
+            <div className="col-md-8">
                 <img src={Imagen} alt="" className='tamaño-imagen'/>
 
             </div>
